b56340ea40f72d1f35ea19cccc8c0dc70445fe3a
bendkaye <46263689+bendkaye@users.noreply.github.com>
2023-02-16 11:44:44 +0000

    changed other route on user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,16 +15,17 @@ export class UserController {
   findAll() {
     return this.userService.findAll();
   }
-}
+
 //   @Get(':id')
 //   findOne(@Param('id') id: string) {
 //     return this.userService.findOne(+id);
 //   }
 
-//   @Patch(':id')
-//   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-//     return this.userService.update(+id, updateUserDto);
-//   }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+}
 
 //   @Delete(':id')
 //   remove(@Param('id') id: string) {
